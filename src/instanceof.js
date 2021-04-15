6741e35d1bad655a333b7f023cc0591ef941c28b
ricksun <ricksun@lexin.com>
2021-04-15 20:12:29 +0800

    手写instanceof方法

--- src/instanceof.js
+++ src/instanceof.js
@@ -1,22 +1,97 @@
-function myInstanceof(object, constructor) {
-  if (typeof constructor !== 'function') {
+/**
+ * instanceof 运算符用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上
+ * object instanceof constructor
+ */
+function myInstanceof(obj, constructor) {
+  var type = typeof constructor
+
+  // constructor must be an object
+  if (type !== 'object' && type !== 'function') {
     throw new TypeError(
       'Right-hand side of ' + "'instanceof'" + ' is not an object'
     )
   }
 
-  var type = typeof object
-  if (type === 'object' && type === 'function') {
+  // constructor must be callable
+  if (type !== 'function') {
+    throw new TypeError(
+      'Right-hand side of ' + "'instanceof'" + ' is not callable'
+    )
+  }
+
+  // obj must be an object, but not null
+  type = typeof obj
+  if ((type !== 'object' && type !== 'function') || obj === null) {
     return false
   }
 
   while (true) {
-    if (object.__proto__ === null) {
+    if (obj.__proto__ === null) {
       return false
     }
-    if (object.__proto__ === constructor.prototype) {
+    if (obj.__proto__ === constructor.prototype) {
       return true
     }
-    object = object.__proto__
+    obj = obj.__proto__
   }
 }
+
+//test case
+function Car(make, model, year) {
+  this.make = make
+  this.model = model
+  this.year = year
+}
+var honda = new Car('Honda', 'Accord', 1998)
+
+honda instanceof Car // true
+myInstanceof(honda,Car) // true
+
+honda instanceof Function // false
+myInstanceof(honda,Function)  // false
+
+honda instanceof Object // true
+myInstanceof(honda,Object)  // true
+
+honda instanceof Date // false
+myInstanceof(honda,Date) // false
+
+Function instanceof Function // true
+myInstanceof(Function,Function) // true
+
+Function instanceof Object // true
+myInstanceof(Function,Object) // true
+
+Object instanceof Object // true
+myInstanceof(Object,Object) // true
+
+Date instanceof Date // false
+myInstanceof(Date,Date) // false
+
+Date instanceof Function // true
+myInstanceof(Date,Function) // true
+
+Date instanceof Object // true
+myInstanceof(Date,Object) // true
+
+'' instanceof String // false
+myInstanceof('',String) // false
+
+[] instanceof [] // Right-hand side of 'instanceof' is not callable
+myInstanceof([],[]) // Right-hand side of 'instanceof' is not callable
+
+[] instanceof '' // Right-hand side of 'instanceof' is not an object
+myInstanceof([],'') // Right-hand side of 'instanceof' is not an object
+
+
+
+
+
+
+
+
+
+
+
+
+
